e4645f4a190633badd5eec8bde726d399865588e
X3N0N <jaredgillam01@gmail.com>
2022-10-12 15:40:57 -0700

    finished linting and organizing

--- node_updater.py
+++ node_updater.py
@@ -3,12 +3,12 @@ from sys import version_info, argv
 from os import path, listdir
 from platform import platform
 from time import sleep
-from .spiffsgen import main as spiffmain
+from spiffsgen import main as spiffmain
 
-#check if non-default dependancies are installed
+#Imported for dependancy checking
 from pkg_resources import get_distribution, DistributionNotFound
 
-#checks to confirm minimum requirements
+#Compatibility Checks
 def version_check():
     if version_info.major != 3:
         print("You must install python 3.x to run this script.")
@@ -16,14 +16,14 @@ def version_check():
         print("\nHalting Program")
         quit()
 
-def platform_check(warn=False):
+def platform_check(safety=True):
     if platform().startswith("darwin") is not True:
         print("This script is not compatible with the current OS installed.")
-        if warn == False:
+        if safety == True:
             print("Halting Program.")
             quit()
         else:
-            print("Platform Check Warning, Program will continue.")
+            print("Platform Check Warning, Program will continue.\n")
 
 def tkinter_check():
     if version_info.minor < 9:
@@ -42,8 +42,34 @@ def tkinter_check():
             tkinter_check()
             quit()
 
+#Arg Handler
+def arg_handler(program, args):
+    run_type = 0
+    if len(args) != 0:
+        if "-h" in args or "--help" in args:
+            help_menu = [
+                "{} Help\n".format(argv[0]),
+                "-h, --help\n",
+                "   Retrieve help for usage of {}\n".format(program),
+                "   Example: python {} --help\n\n".format(program),
 
-def node_locating(silent_stat=True):
+                "-f, --flash\n",
+                "   Run the script in Live mode and flash the Fiberpunk Node\n",
+                "   Example: python {} --flash\n".format(program)
+                ]
+            print("".join(help_menu))
+            quit()
+
+        elif "-f" in args or "--flash" in args:
+            run_type = 1
+
+        else:
+            print("Invalid argument.\nSee `python {} -h` for details.".format(program))
+            quit()
+    return run_type
+
+#Operating Functions
+def node_locating(verbose=True):
     target_dev = []
     ports = list(list_ports.comports())
     for port in ports:
@@ -60,7 +86,7 @@ def node_locating(silent_stat=True):
     elif len(target_dev) == 1:
         port = target_dev[0]
         print("Node Found on port: {}\n".format(target_dev[0].device))
-        if silent_stat is False:
+        if verbose is True:
             print("###### {} ######".format(port.device))
             print("Device: %s" % port.device)
             print("Name: %s" % port.name)
@@ -98,10 +124,10 @@ def firmware_selection():
         if firmware_hash in approved_hashes:
             return dialog
         else:
-            print("{} is not a valid firmware file.\nHalting Program")
+            print("{} is not a valid firmware file.\nHalting Program".format(path.basename(dialog)))
             quit()
 
-def webpage_selection():
+def webpage_selection(): #Unused (Might use in later versions)
     dialog = askdirectory(title="Select Node Webpage Folder")
     if not dialog:
         print("No Webpage Folder Selected.\nHalting Program")
@@ -115,45 +141,34 @@ def webpage_selection():
 
 
 def main():
-    args = argv[1:]
-    run_type = 0
-    if len(args) != 0:
-        if "-h" in args or "--help" in args:
-            help_menu = [
-                "{} Help\n".format(argv[0]),
-                "-h, --help\n",
-                "   Retrieve help for usage of {}\n".format(argv[0]),
-                "   Example: python {} --help\n\n".format(argv[0]),
-
-                "-f, --flash\n",
-                "   Run the script in Live mode and flash the Fiberpunk Node\n",
-                "   Example: python {} --flash\n".format(argv[0])
-                ]
-            print("".join(help_menu))
-            quit()
-
-        elif "-f" in args or "--flash" in args:
-            run_type = 1
-
-        else:
-            print("Invalid argument.\nSee `python {} -h` for details.".format(argv[0]))
-            quit()
+    run_type = arg_handler(argv[0], argv[1:]) #return 0 for testing, 1 for live (To Flash Firmware)
 
     required_info = {"Port":None, "Firmware":None, "Webpage":None}
-    tkinter_check()
 
-    required_info["Port"] = node_locating(silent_stat=False)
+    required_info["Port"] = node_locating(switchboard["node_locating"])
     required_info["Firmware"] = path.basename(firmware_selection())
-    required_info["Webpage"] = "webpage"#webpage_selection()
+    required_info["Webpage"] = "webpage"
 
-    print("\nPreparing to Flash Node Firmware...\nDO NOT UNPLUG DEVICE UNTIL COMPLETE")
-    sleep(1)
+    print("\nPreparing to Flash Node\nDO NOT UNPLUG DEVICE UNTIL COMPLETE")
+    sleep(2) #Sleeping so user can read warning
     
+    # SPIFF GENERATION
+    print("Generating Spiff BIN file (Step 1/3)")
+    try:
+        spiffmain(['1507328', required_info["Webpage"], 'spifs.bin'])
+    except Exception as e:
+        print("Unable to generate spifs.bin due to:\n{}\nHalting Program".format(e))
+        quit()   
+    print("Spiff BIN file Successfully generated!\n")
+
+    # MAIN FIRMWARE FLASHING
+    print("Flashing Node Main Firmware (Step 2/3)")
     command = ["--chip", "esp32", "--baud", "921600", "--port", required_info["Port"], "--before", "default_reset", "--after", "hard_reset", 
     "write_flash", "-z", "--flash_mode", "dio", "--flash_freq", "80m", "--fresh_size", "detect", "0xe000", "boot_app0.bin", 
     "0x1000", "bootloader_qio_80m.bin", "0x8000", "default.bin", "0x10000", required_info["Firmware"]]
 
-    print('Using command %s' % ' '.join(command))
+    if switchboard["list_flash_info"] is True:
+        print('Using command %s' % ' '.join(command))
     try:
         if run_type == 1:
             esptool.main(command)
@@ -162,18 +177,16 @@ def main():
     except Exception as e:
         print("Flashing Error Occurred:\n↳  {}".format(e))
         quit()
-    print("Flashing of Firmware Complete\n")
-    print("Generating Spiff BIN file...")
-    try:
-        spiffmain(['1507328', required_info["Webpage"], 'spifs.bin'])
-    except Exception as e:
-        print("Unable to generate spifs.bin due to:\n{}\nHalting Program".format(e))
-        quit()
-    
-    print("Spiff BIN file Successfully generated!\n")
-    print("Flashing Webpage BIN file...")
+    print("Flashing of Main Firmware Complete\nDO NOT UNPLUG DEVICE")
+    sleep(2) #Sleeping so user can read Success
+
+    # WEBPAGE FLASHING
+    print("Flashing Webpage BIN file (Stage 3/3)")
     command = ["--chip", "esp32", "--baud", "921600", "--port", required_info["Port"], "--before", "default_reset", "--after", "hard_reset", "write_flash", "-z",
     "--flash_mode", "dio", "--flash_freq", "80m", "--fresh_size", "detect", "2686976", "spifs.bin"]
+
+    if switchboard["list_flash_info"] is True:
+        print('Using command %s' % ' '.join(command))
     try:
         if run_type == 1:
             esptool.main(command)
@@ -184,21 +197,47 @@ def main():
         quit()
     
     if run_type == 1:
-        print("\n\nNODE UPDATE COMPLETE!!!")
+        print("\n\nNODE UPDATE COMPLETE!!!\nIt is safe to unplug the Node")
     else:
-        print("\n\nNODE TEST COMPLETE!!!")
+        print("\n\nNODE TEST COMPLETE!!!\nIt is safe to unplug the Node")
+
+
 
 if __name__ == "__main__":
+    #Default Switchboard {"platform_check":True, "node_locating":False, "list_installed":False, "list_flash_info":False}
+    switchboard = {"platform_check":True, "node_locating":False, "list_installed":False, "list_flash_info":False}
+    """
+    "platform_check" Switchboard bool is if the program should quit on non-compatible OS
+    "node_locating" Switchboard bool is if the program should verbosely list port info
+    "list_installed" Switchboard bool is if the program should list installed dependancies
+    "list_flash_info" Switchboard bool is if the program should print the flashing payload
+    """
+
+    #Version Check
+    version_check()
+    #Tkinter Check
+    tkinter_check()
+    #Platform Check
+    platform_check(switchboard["platform_check"])
+
+    ##DEPENDANCY CHECKING
     reqs = []
     for line in open("requirements.txt", "r").readlines():
         reqs.append(line.strip())
+    success_import = True
     for package in reqs:
         try:
             dist = get_distribution(package)
-            print('{} ({}) is installed'.format(dist.key, dist.version))
+            if switchboard["list_installed"] is True:
+                print('{} ({}) is installed'.format(dist.key, dist.version))
         except DistributionNotFound:
             print('{} is NOT installed'.format(package))
-
+            success = False
+    if success_import is False:
+        print("Missing the above dependancies. Please install them before running this script")
+        quit()
+    print("")
+    #Import Non-Core Libs post
     try:
         from tkinter.filedialog import askopenfilename, askdirectory
         from serial.tools import list_ports
